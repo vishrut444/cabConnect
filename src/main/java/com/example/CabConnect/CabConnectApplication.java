51e4863763b629450514eb9d42eca81e45af6580
Vishrut <varshikar@Vishruts-MacBook-Air.local>
2024-06-12 21:43:30 +0530

    cabConnect-1

---
+++ src/main/java/com/example/CabConnect/CabConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CabConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CabConnect/CabConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CabConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
